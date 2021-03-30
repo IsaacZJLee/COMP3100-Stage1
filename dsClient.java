c1cb18674b1aa8fa8652e0c4176d3eaec9cbdcbc
staycool2020 <68573753+staycool2020@users.noreply.github.com>
2021-03-30 23:27:14 +1100

    Configured dsClient Class
    Added dsClient variables and import libraries.

---
+++ dsClient.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class dsClient {
+        public static Server[] readXML(Server[] serverArr){
+        try {
+			File systemXML = new File("/home/rhynefong/Desktop/ds-sim/src/pre-compiled/ds-system.xml");
+
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(systemXML);
+
+			doc.getDocumentElement().normalize();
+			NodeList dsServers = doc.getElementsByTagName("server");
+			serverArr = new Server[dsServers.getLength()];
+        }
+}
