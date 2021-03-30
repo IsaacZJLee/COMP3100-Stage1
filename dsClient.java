7bf1f1cefe8006152ebffa774976100433cbeb79
45559708 <53924643+45559708@users.noreply.github.com>
2021-03-30 23:57:34 +1100

    Append dsClient class
    Added dsClient constructor and inherited it from dsServer class.

--- dsClient.java
+++ dsClient.java
@@ -8,9 +8,18 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-public class dsClient {
-        public static Server[] readXML(Server[] dsServerArray){
-        try {
+public class dsClient extends dsServer {
+
+	dsClient(int sID, String type, int limit, int bootupTime, float hourlyRate, int coreCount, int memory, int disk) {
+		super(sID, type, limit, bootupTime, hourlyRate, coreCount, memory, disk);
+
+	}
+
+	public static int largestServer = 0;
+
+	public static dsServer[] readXML(dsServer[] dsServerArray) {
+
+		try {
 			File systemXML = new File("/home/rhynefong/Desktop/ds-sim/src/pre-compiled/ds-system.xml");
 
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -19,6 +28,31 @@ public class dsClient {
 
 			doc.getDocumentElement().normalize();
 			NodeList dsServers = doc.getElementsByTagName("server");
-			dsServerArray = new Server[dsServers.getLength()];
-        }
+			dsServerArray = new dsServer[dsServers.getLength()];
+
+			for (int i = 0; i < dsServers.getLength(); i++) {
+				Element dsServer = (Element) dsServers.item(i);
+				String type = dsServer.getAttribute("type");
+				int limit = Integer.parseInt(dsServer.getAttribute("limit"));
+				int bootupTime = Integer.parseInt(dsServer.getAttribute("bootupTime"));
+				float hourlyRate = Float.parseFloat(dsServer.getAttribute("hourlyRate"));
+				int coreCount = Integer.parseInt(dsServer.getAttribute("coreCount"));
+				int memory = Integer.parseInt(dsServer.getAttribute("memory"));
+				int disk = Integer.parseInt(dsServer.getAttribute("disk"));
+				System.out.println(type);
+
+				dsServer tempArr = new dsServer(i, type, limit, bootupTime, hourlyRate, coreCount, memory, disk);
+				dsServerArray[i] = tempArr;
+			}
+
+			largestServer = setLargestServer(dsServerArray);
+		}
+
+		catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("ERROR DETECTED!!");
+		}
+
+		return dsServerArray;
+	}
 }
