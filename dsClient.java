14b9a84cfc38cd30e458174f35cdcef5edcf31a7
isaac-lee-zhuan-jian.isaac-@students.mq.edu.au <53724604+IsaacZJMQ@users.noreply.github.com>
2021-04-18 23:26:36 +1000

    Updated dsClient.java
    Added readDSXML

--- dsClient.java
+++ dsClient.java
@@ -45,5 +45,38 @@ public class dsClient {
         }
     }
 
+	public static void readDSXML() {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(dsServerXML);
+
+            doc.getDocumentElement().normalize();
+            NodeList servers = doc.getElementsByTagName("server");
+
+            for (int i = 0; i < servers.getLength(); i++) { // reading xml file and passing data into the correct variables
+                Element server = (Element) servers.item(i);
+                for (int j = 0; j < Integer.parseInt(server.getAttribute("limit")); j++) {
+                    String type = server.getAttribute("type");
+                    int limit = Integer.parseInt(server.getAttribute("limit"));
+                    int bootupTime = Integer.parseInt(server.getAttribute("bootupTime"));
+                    float hourlyRate = Float.parseFloat(server.getAttribute("hourlyRate"));
+                    int coreCount = Integer.parseInt(server.getAttribute("coreCount"));
+                    int memory = Integer.parseInt(server.getAttribute("memory"));
+                    int disk = Integer.parseInt(server.getAttribute("disk"));
+
+                    /create server object to store all properties from the xml file/
+
+                    dsServer server_obj = new dsServer(j, type, limit, bootupTime, hourlyRate, coreCount, memory, disk); 
+                    dsServerArray.add(server_obj); // add server object to dsServerArray
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 	
 }
