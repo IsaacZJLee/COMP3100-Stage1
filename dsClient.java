f45adbdc959bf40b6489b85a5401e49bec09f544
45559708 <53924643+45559708@users.noreply.github.com>
2021-04-18 22:42:16 +1000

    Added more attributes to dsClient.java

--- dsClient.java
+++ dsClient.java
@@ -1,17 +1,36 @@
-import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+public class dsClient {
+	// declaring dsCLient fields
+	public static String host_name = "localhost";
+	public static int server_Port = 50000;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+	public static String username = "group_18";
+	public static String HELO = "HELO";
+	public static String AUTH = "AUTH";
+	public static String AUTH_username = AUTH + " " + username;
+	public static String OK = "OK";
+	public static String REDY = "REDY";
+	public static String JOBN = "JOBN"; // Job submission information
+	public static String SCHD = "SCHD"; // Scheduling decision
+	public static String JOBP = "JOBP";
+	public static String JCPL = "JCPL"; // Job completion
+	public static String QUIT = "QUIT";
 
-public class dsClient extends dsServer {
+	public static char[] charServerMsg; // contains current server message in the form of chars
+	public static int charSMsgSize = 100;
+
+	public static String stringServerMsg; // contains the current server message in the form of String
+	public static byte[] byteServerMsg; // contains current server message in the form of bytes
+	public static String[] fieldServerMsg; // contains current server message in the form of the individual Strings in
+											// the array
+	public static ArrayList<dsServer> dsServerArray; // stores information from the server
 
 	dsClient(int sID, String type, int limit, int bootupTime, float hourlyRate, int coreCount, int memory, int disk) {
-		super(sID, type, limit, bootupTime, hourlyRate, coreCount, memory, disk);
 
 	}
 
